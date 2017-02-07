8fc2267fa24f7b14c1e5b1a38a5ffdf8209c7b2b
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-07 15:25:54 -0800

    done

--- items/cd.py
+++ items/cd.py
@@ -1,4 +1,8 @@
 from .item import Item
 
 class Cd(Item):
-    pass
+  def __init__(self, name, price, artist, tracks, runtime):
+      super().__init__(name, price)
+      self.artist = artist
+      self.tracks = tracks
+      self.runtime = runtime
